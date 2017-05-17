1a606704fa1eec5e11dde8691af2b2c35469083c
moti <moti@practitest.com>
2017-05-17 22:08:09 +0700

    parse request id and poll for results

--- js/background.js
+++ js/background.js
@@ -1,5 +1,5 @@
 // const SEARCH_ENDPOINT = "https://fathomless-tundra-65369.herokuapp.com/search"
-const SEARCH_ENDPOINT = "http://127.0.0.1:8080/search"
+const SEARCH_ENDPOINT = "http://127.0.0.1:8080/"
 var historyObjectsCache;
 
 function fetchAllHistory(){
@@ -12,24 +12,51 @@ function fetchAllHistory(){
   })
 }
 
-function fetchPostInit(content){
-  var headers = new Headers({
+function fetchInit(method,content){
+  const headers = new Headers({
     'Content-Type': 'application/json'
   })
-  return { method: 'POST',
+  return { method: method,
                headers: headers,
                mode: 'cors',
                cache: 'default',
-               body: JSON.stringify(content)}
+               body: content ? JSON.stringify(content) : undefined}
 }
 
 function filterHistory(query, historyObjs){
-  fetch(SEARCH_ENDPOINT,fetchPostInit(historyObjs))
+  const urls = historyObjs.map(o => o.url)
+  console.dir({query, urls})
+  fetch(SEARCH_ENDPOINT, fetchInit('POST',{query, urls}))
     .then(function(result){
-      debugger
+      if(result.status == 200){
+        const request_id = result.body
+        console.info(request_id)
+
+      }
     }).catch(function(error){
-      debugger
+      console.error(error);
     })
+}
+
+function pollingResults(request_id){
+
+  return new Promise(function(resolve, reject){
+    const pollingIntervalId = setInterval(pollResults, 1000)
+    var requestsCounter = 0
+    const MAX_REQUESTS = 200
+    function pollResults(req_id){
+      fetch(SEARCH_ENDPOINT + req_id,fetchInit('GET')).then((result)=> {
+        console.dir(result)
+        if(result.status == 200){
+          clearInterval(pollingIntervalId)
+          resolve(result.body)
+        }else if(result.status != 404 || requestsCounter > MAX_REQUESTS){
+          clearInterval(pollingIntervalId)
+          reject(result)
+        }
+      })
+    }
+  })
 }
 
 chrome.omnibox.onInputStarted.addListener((text, suggest) => {
